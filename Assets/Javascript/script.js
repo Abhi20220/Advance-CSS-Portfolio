06de4cc26d002b0de1dea93119e9cd23120519e2
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-03-16 12:26:04 +1030

    added about-section and finished details

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -11,6 +11,9 @@ $(document).ready(function(){
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     });
+    
 }); 
 
+  
